d79ff99a762d18fc98e77ecfbebbc368241626da
whli745 <623374047@qq.com>
2019-04-07 08:15:08 +0800

    init commit

---
+++ autumn-config/src/main/java/com/whli/autumn/config/ConfigApplication.java
@@ -0,0 +1,20 @@
+package com.whli.autumn.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * <p>分布式配置中心</p>
+ * @author whli
+ * @date 2019/4/7 8:01
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class,args);
+    }
+}
---
+++ autumn-eureka/src/main/java/com/whli/autumn/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.whli.autumn.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * <p>eureka服务发现与注册中心</p>
+ * @author whli
+ * @date 2019/4/7 7:56
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
